bb061a79be8d9c87d537ea553e5b012be1a4abfa
Patrick Scott <patrick.lee.scott@gmail.com>
2015-07-16 00:37:04 -0400

    simply intro css

--- site/client/pages/Information/Information.js
+++ site/client/pages/Information/Information.js
@@ -1,3 +1,13 @@
 Template.Information.rendered = function () {
   App.UI.fullScreenContainer(this.$('.full-screen-container'));
 };
+
+var $onResize = function(event) {
+  App.UI.fullScreenContainer($('.full-screen-container'));
+}
+
+var debouncedOnResize = _.debounce($onResize, 300);
+
+Meteor.startup(function() {
+  $(window).on('resize', debouncedOnResize);
+});
