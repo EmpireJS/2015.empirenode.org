9a9c5f3d2bd9983ca9a71a72f78e5c0e600f8ae0
Patrick Scott <patrick.lee.scott@gmail.com>
2015-09-08 07:33:22 -0400

    dont show cfp popup on mobile

--- site/client/pages/Information/Information.js
+++ site/client/pages/Information/Information.js
@@ -1,10 +1,10 @@
 Template.Information.rendered = function () {
   App.UI.fullScreenContainer(this.$('.full-screen-container'));
-  if (ResponsiveHelpers.isXs()) {
-    Meteor.setTimeout(function() {
-      sAlert.info('<a href="https://docs.google.com/forms/d/1qUgKrfhuAOpsmqCE-9blPOyd2m7WjeuaO7qBdDdmARM" target="_blank" class="default-color">Call for Speakers is now Open!</a>', {html: true});
-    }, 2000);
-  }
+  // if (ResponsiveHelpers.isXs()) {
+  //   Meteor.setTimeout(function() {
+  //     sAlert.info('<a href="https://docs.google.com/forms/d/1qUgKrfhuAOpsmqCE-9blPOyd2m7WjeuaO7qBdDdmARM" target="_blank" class="default-color">Call for Speakers is now Open!</a>', {html: true});
+  //   }, 2000);
+  // }
 
 };
 
