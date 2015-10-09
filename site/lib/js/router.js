648306ec54a4f3594858c6c5b736c2021ca5802a
Matt Walters <mattwalters5@gmail.com>
2015-10-08 21:42:29 -0400

    adding reading redirects for newspaper

--- site/lib/js/router.js
+++ site/lib/js/router.js
@@ -62,6 +62,31 @@ Router.route('/getting-here', function () {
   }
 });
 
+var redirects = {
+  '1': 'https://iojs.org/en/faq.html',
+  '2': 'http://www.joyent.com/about/press/joyent-moves-to-establish-nodejs-foundation',
+  '3': 'http://venturebeat.com/2015/05/13/node-project-spinoff-io-js-is-moving-to-the-node-js-foundation-with-a-merge-in-progress/',
+  '4': 'http://www.cli-nerd.com/2015/09/09/7-reasons-to-upgrade-to-node-v4-now.html',
+  '5': 'http://cylonjs.com/blog/2015/04/08/cylon-1.0.0/',
+  '6': 'https://medium.com/@rvagg/node-js-io-js-state-of-the-build-arm-2f24c18e1ab4',
+  '7': 'http://radar.oreilly.com/2015/06/the-smartest-way-to-program-smart-things-node-js.html',
+  '8': 'http://www.bennadel.com/blog/2792-shedding-the-monolithic-application-with-aws-simple-queue-service-sqs-and-node-js.htm',
+  '9': 'https://vimeo.com/136965797',
+  '10': 'https://github.com/phusion/passenger-docker',
+  '11': 'http://www.toptal.com/nodejs/debugging-memory-leaks-node-js-applications',
+  '12': 'http://www.nearform.com/nodecrunch/self-detect-memory-leak-node/',
+  '13': 'http://www.toptal.com/nodejs/top-10-common-nodejs-developer-mistakes',
+  '14': 'http://javascriptplayground.com/blog/2015/03/node-command-line-tool/',
+  '15': 'https://medium.com/startup-study-group/40-npm-modules-we-can-t-live-without-36e29e352e3a',
+  '16': 'http://blog.risingstack.com/graphql-overview-getting-started-with-graphql-and-nodejs/',
+  '17': 'http://thejackalofjavascript.com/dna-analysis-node-js/'
+};
 
-
-
+Router.route('/r/:id', {
+  action: function (bla) {
+    console.log(this.params.id);
+    var url = redirects[this.params.id];
+    if (url) window.location = url;
+    else Router.go('/');
+  }
+});
